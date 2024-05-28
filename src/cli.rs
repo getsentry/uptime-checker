f0ed74f91ea98769b48bb049ddc84c88956afa92
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-05-28 11:23:45 -0400

    Add do nothing tokio
    Just waits for ^c

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,16 @@
-use crate::cliapp::make_app;
-use anyhow::Result;
+use std::io;
 
-pub fn execute() -> Result<()> {
+use crate::cliapp::make_app;
+
+use tokio::signal::ctrl_c;
+
+pub async fn execute() -> io::Result<()> {
     let app = make_app();
     let matches = app.get_matches();
 
     if let Some(_matches) = matches.subcommand_matches("run") {
-        println!("Hello world");
-        Ok(())
+        println!("Hello world. I am doing nothing. ^C to exit");
+        ctrl_c().await
     } else {
         unreachable!();
     }
