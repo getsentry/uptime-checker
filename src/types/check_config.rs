dd75ec6e3abca6997f36bc501f95e1389a7bd544
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-06-27 17:01:15 -0400

    feat(check-config): Support msgpack deseraizlization (#80)

--- src/types/check_config.rs
+++ src/types/check_config.rs
@@ -1,10 +1,14 @@
 use chrono::TimeDelta;
+use serde::Deserialize;
+use serde_repr::Deserialize_repr;
+use serde_with::serde_as;
 use uuid::Uuid;
 
 const ONE_MINUTE: isize = 60;
 
 /// Valid intervals between the checks
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[repr(isize)]
+#[derive(Debug, Copy, Clone, PartialEq, Deserialize_repr)]
 pub enum CheckInterval {
     OneMinute = ONE_MINUTE,
     FiveMinutes = ONE_MINUTE * 5,
@@ -18,15 +22,20 @@ pub enum CheckInterval {
 pub const MAX_CHECK_INTERVAL_SECS: usize = CheckInterval::SixtyMinutes as usize;
 
 /// The CheckConfig represents a configuration for a single check.
-#[derive(Debug, PartialEq, Clone)]
+#[serde_as]
+#[derive(Debug, PartialEq, Clone, Deserialize)]
 pub struct CheckConfig {
     /// The subscription this check configuration is associated to in sentry.
+    #[serde(with = "uuid::serde::simple")]
     pub subscription_id: Uuid,
 
     /// The interval between each check run.
+    #[serde(rename = "interval_seconds")]
     pub interval: CheckInterval,
 
     // The total time we will allow to make the request in.
+    #[serde(rename = "timeout_ms")]
+    #[serde_as(as = "serde_with::DurationMilliSeconds<i64>")]
     pub timeout: TimeDelta,
 
     /// The actual HTTP URL to check.
@@ -58,7 +67,7 @@ mod tests {
 
     use chrono::TimeDelta;
     use similar_asserts::assert_eq;
-    use uuid::Uuid;
+    use uuid::{uuid, Uuid};
 
     use crate::types::check_config::MAX_CHECK_INTERVAL_SECS;
 
@@ -73,6 +82,32 @@ mod tests {
                 url: "https://example.com".to_string(),
             }
         }
+    }
+
+    #[test]
+    fn test_serialize_msgpack_roundtrip() {
+        // Example msgpack taken from
+        // sentry-kafka-schemas/examples/uptime-configs/1/example.msgpack
+        let example = vec![
+            0x84, 0xaf, 0x73, 0x75, 0x62, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e,
+            0x5f, 0x69, 0x64, 0xd9, 0x20, 0x64, 0x37, 0x36, 0x32, 0x39, 0x63, 0x36, 0x63, 0x38,
+            0x32, 0x62, 0x65, 0x34, 0x66, 0x36, 0x37, 0x39, 0x65, 0x65, 0x37, 0x38, 0x61, 0x30,
+            0x64, 0x39, 0x37, 0x37, 0x38, 0x35, 0x36, 0x64, 0x32, 0xa3, 0x75, 0x72, 0x6c, 0xb0,
+            0x68, 0x74, 0x74, 0x70, 0x3a, 0x2f, 0x2f, 0x73, 0x65, 0x6e, 0x74, 0x72, 0x79, 0x2e,
+            0x69, 0x6f, 0xb0, 0x69, 0x6e, 0x74, 0x65, 0x72, 0x76, 0x61, 0x6c, 0x5f, 0x73, 0x65,
+            0x63, 0x6f, 0x6e, 0x64, 0x73, 0xcd, 0x01, 0x2c, 0xaa, 0x74, 0x69, 0x6d, 0x65, 0x6f,
+            0x75, 0x74, 0x5f, 0x6d, 0x73, 0xcd, 0x01, 0xf4,
+        ];
+
+        assert_eq!(
+            rmp_serde::from_slice::<CheckConfig>(&example).unwrap(),
+            CheckConfig {
+                subscription_id: uuid!("d7629c6c-82be-4f67-9ee7-8a0d977856d2"),
+                timeout: TimeDelta::milliseconds(500),
+                interval: CheckInterval::FiveMinutes,
+                url: "http://sentry.io".to_string(),
+            }
+        );
     }
 
     #[test]
