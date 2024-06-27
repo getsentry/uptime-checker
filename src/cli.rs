789659bf3b8778c6bdc4bc9d1193940d5f59a31f
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-06-26 20:50:11 -0400

    feat: Schedule executions from the ConfigStore (#73)
    Connects the `ConfigStore` to the scheduler.
    
    The scheduler will now read all `CheckConfig`s at the given tick and
    create futures to execute each check.
    
    I've also removed the `tokio-cron-scheduler` package in favor of using
    `tokio`s build in `Interval` to generate our ticks
    
    The example config has now moved the the root cli runner for now

--- src/cli.rs
+++ src/cli.rs
@@ -1,14 +1,17 @@
-use std::io;
+use std::{io, sync::Arc, time::Duration};
 
 use clap::Parser;
 use tokio::signal::ctrl_c;
 use tracing::info;
+use uuid::uuid;
 
 use crate::{
     cliapp::{CliApp, Commands},
     config::Config,
+    config_store::ConfigStore,
     logging::{self, LoggingConfig},
     scheduler::run_scheduler,
+    types::check_config::{CheckConfig, CheckInterval},
 };
 
 pub fn execute() -> io::Result<()> {
@@ -19,13 +22,23 @@ pub fn execute() -> io::Result<()> {
 
     info!(config = ?config);
 
+    let mut config_store = ConfigStore::new();
+
+    // XXX: Example config while we build out the consumer that loads configs
+    config_store.add_config(Arc::new(CheckConfig {
+        url: "https://downtime-simulator-test1.vercel.app".to_string(),
+        subscription_id: uuid!("663399a09e6340a79c3c7a3f26878904"),
+        interval: CheckInterval::FiveMinutes,
+        timeout: Duration::from_secs(5),
+    }));
+
     match app.command {
         Commands::Run => tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .build()
             .unwrap()
             .block_on(async {
-                run_scheduler(&config)
+                run_scheduler(&config, Arc::new(config_store))
                     .await
                     .expect("Failed to run scheduler");
                 ctrl_c().await
