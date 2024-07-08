164b62352919fb8509888fec010b6ea3bf31748c
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-07-08 12:13:07 -0400

    ref: Add traits for Checker and ResultsProducer (#95)
    Will allow us to provide different produers and chekcers in tests

---
+++ src/checker/http_checker.rs
@@ -0,0 +1,330 @@
+use chrono::{TimeDelta, Utc};
+use reqwest::{Client, ClientBuilder, Response, StatusCode};
+use sentry::protocol::{SpanId, TraceId};
+use std::error::Error;
+use tokio::time::Instant;
+use uuid::Uuid;
+
+use crate::config_store::Tick;
+use crate::types::{
+    check_config::CheckConfig,
+    result::{
+        CheckResult, CheckStatus, CheckStatusReason, CheckStatusReasonType, RequestInfo,
+        RequestType,
+    },
+};
+
+use super::Checker;
+
+/// Responsible for making HTTP requests to check if a domain is up.
+#[derive(Clone, Debug)]
+pub struct HttpChecker {
+    client: Client,
+}
+
+/// Fetches the response from a URL.
+///
+/// First attempts to fetch just the head, and if not supported falls back to fetching the entire body.
+async fn do_request(
+    client: &Client,
+    check_config: &CheckConfig,
+    sentry_trace: &str,
+) -> (RequestType, Result<Response, reqwest::Error>) {
+    let timeout = check_config
+        .timeout
+        .to_std()
+        .expect("Timeout duration could not be converted to std::time::Duration");
+
+    let head_response = match client
+        .head(&check_config.url)
+        .timeout(timeout)
+        .header("sentry-trace", sentry_trace.to_owned())
+        .send()
+        .await
+    {
+        Ok(response) => response,
+        Err(e) => return (RequestType::Head, Err(e)),
+    };
+
+    // Successful head request
+    if head_response.status() != StatusCode::METHOD_NOT_ALLOWED {
+        return (RequestType::Head, Ok(head_response));
+    }
+
+    let result = client
+        .get(check_config.url.as_str())
+        .timeout(timeout)
+        .header("sentry-trace", sentry_trace.to_owned())
+        .send()
+        .await;
+
+    (RequestType::Get, result)
+}
+
+/// Check if the request error is a DNS error.
+fn dns_error(err: &reqwest::Error) -> Option<String> {
+    let mut inner = &err as &dyn Error;
+    while let Some(source) = inner.source() {
+        inner = source;
+
+        // TODO: Would be better to get specific errors without string matching like this
+        // Not sure if there's a better way
+        let inner_message = inner.to_string();
+        if inner_message.contains("dns error") {
+            return Some(inner.source().unwrap().to_string());
+        }
+    }
+    None
+}
+
+impl HttpChecker {
+    pub fn new() -> Self {
+        let client = ClientBuilder::new()
+            .build()
+            .expect("Failed to build checker client");
+
+        Self { client }
+    }
+}
+
+impl Checker for HttpChecker {
+    /// Makes a request to a url to determine whether it is up.
+    /// Up is defined as returning a 2xx within a specific timeframe.
+    async fn check_url(&self, config: &CheckConfig, tick: &Tick) -> CheckResult {
+        let scheduled_check_time = tick.time();
+        let actual_check_time = Utc::now();
+
+        let trace_id = TraceId::default();
+        let span_id = SpanId::default();
+
+        // Format the 'sentry-trace' header. The last byte indicates that we are NOT forcing
+        // sampling on child spans. If we were to set this to '1' it would mean that every
+        // check request made for that customer would be sampled,
+        let trace_header = format!("{}-{}-{}", trace_id, span_id, '0');
+
+        let start = Instant::now();
+        let (request_type, response) = do_request(&self.client, config, &trace_header).await;
+        let duration = Some(TimeDelta::from_std(start.elapsed()).unwrap());
+
+        let status = if response.as_ref().is_ok_and(|r| r.status().is_success()) {
+            CheckStatus::Success
+        } else {
+            CheckStatus::Failure
+        };
+
+        let http_status_code = match &response {
+            Ok(r) => Some(r.status().as_u16()),
+            Err(e) => e.status().map(|s| s.as_u16()),
+        };
+
+        let request_info = Some(RequestInfo {
+            http_status_code,
+            request_type,
+        });
+
+        let status_reason = match response {
+            Ok(r) if r.status().is_success() => None,
+            Ok(r) => Some(CheckStatusReason {
+                status_type: CheckStatusReasonType::Failure,
+                description: format!("Got non 2xx status: {}", r.status()),
+            }),
+            Err(e) => Some({
+                if e.is_timeout() {
+                    CheckStatusReason {
+                        status_type: CheckStatusReasonType::Timeout,
+                        description: format!("{:?}", e),
+                    }
+                } else if let Some(message) = dns_error(&e) {
+                    CheckStatusReason {
+                        status_type: CheckStatusReasonType::DnsError,
+                        description: message,
+                    }
+                } else {
+                    CheckStatusReason {
+                        status_type: CheckStatusReasonType::Failure,
+                        description: format!("{:?}", e),
+                    }
+                }
+            }),
+        };
+
+        CheckResult {
+            guid: Uuid::new_v4(),
+            subscription_id: config.subscription_id,
+            status,
+            status_reason,
+            trace_id,
+            span_id,
+            scheduled_check_time,
+            actual_check_time,
+            duration,
+            request_info,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::checker::Checker;
+    use crate::config_store::Tick;
+    use crate::types::check_config::CheckConfig;
+    use crate::types::result::{CheckStatus, CheckStatusReasonType, RequestType};
+
+    use super::HttpChecker;
+    use chrono::{TimeDelta, Utc};
+    use httpmock::prelude::*;
+    use httpmock::Method;
+    use reqwest::StatusCode;
+
+    fn make_tick() -> Tick {
+        Tick::from_time(Utc::now() - TimeDelta::seconds(60))
+    }
+
+    #[tokio::test]
+    async fn test_simple_head() {
+        let server = MockServer::start();
+        let checker = HttpChecker::new();
+
+        let duration = TimeDelta::milliseconds(50);
+
+        let head_mock = server.mock(|when, then| {
+            when.method(Method::HEAD).path("/head");
+            then.delay(duration.to_std().unwrap()).status(200);
+        });
+
+        let config = CheckConfig {
+            url: server.url("/head").to_string(),
+            ..Default::default()
+        };
+
+        let tick = make_tick();
+        let result = checker.check_url(&config, &tick).await;
+
+        assert_eq!(result.status, CheckStatus::Success);
+        assert!(result.status_reason.is_none());
+        assert_eq!(
+            result.request_info.as_ref().map(|i| i.request_type),
+            Some(RequestType::Head)
+        );
+        assert_eq!(
+            result.request_info.and_then(|i| i.http_status_code),
+            Some(200)
+        );
+        assert_eq!(result.scheduled_check_time, tick.time());
+        assert!(result.actual_check_time > tick.time());
+        assert!(result.duration.is_some_and(|d| d >= duration));
+
+        head_mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_simple_get() {
+        let server = MockServer::start();
+        let checker = HttpChecker::new();
+
+        let head_disallowed_mock = server.mock(|when, then| {
+            when.method(Method::HEAD).path("/no-head");
+            then.status(StatusCode::METHOD_NOT_ALLOWED.as_u16());
+        });
+        let get_mock = server.mock(|when, then| {
+            when.method(Method::GET)
+                .path("/no-head")
+                .header_exists("sentry-trace");
+            then.status(200);
+        });
+
+        let config = CheckConfig {
+            url: server.url("/no-head").to_string(),
+            ..Default::default()
+        };
+
+        let tick = make_tick();
+        let result = checker.check_url(&config, &tick).await;
+
+        assert_eq!(result.status, CheckStatus::Success);
+        assert_eq!(
+            result.request_info.as_ref().map(|i| i.request_type),
+            Some(RequestType::Get)
+        );
+
+        head_disallowed_mock.assert();
+        get_mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_simple_timeout() {
+        static TIMEOUT: i64 = 200;
+
+        let server = MockServer::start();
+
+        let timeout = TimeDelta::milliseconds(TIMEOUT);
+        let checker = HttpChecker::new();
+
+        let timeout_mock = server.mock(|when, then| {
+            when.method(Method::HEAD)
+                .path("/timeout")
+                .header_exists("sentry-trace");
+            then.delay((timeout + TimeDelta::milliseconds(200)).to_std().unwrap())
+                .status(200);
+        });
+
+        let config = CheckConfig {
+            url: server.url("/timeout").to_string(),
+            timeout,
+            ..Default::default()
+        };
+
+        let tick = make_tick();
+        let result = checker.check_url(&config, &tick).await;
+
+        assert_eq!(result.status, CheckStatus::Failure);
+        assert!(result.duration.is_some_and(|d| d > timeout));
+        assert_eq!(result.request_info.and_then(|i| i.http_status_code), None);
+        assert_eq!(
+            result.status_reason.map(|r| r.status_type),
+            Some(CheckStatusReasonType::Timeout)
+        );
+
+        timeout_mock.assert();
+    }
+
+    #[tokio::test]
+    async fn test_simple_400() {
+        let server = MockServer::start();
+        let checker = HttpChecker::new();
+
+        let head_mock = server.mock(|when, then| {
+            when.method(Method::HEAD)
+                .path("/head")
+                .header_exists("sentry-trace");
+            then.status(400);
+        });
+
+        let config = CheckConfig {
+            url: server.url("/head").to_string(),
+            ..Default::default()
+        };
+
+        let tick = make_tick();
+        let result = checker.check_url(&config, &tick).await;
+
+        assert_eq!(result.status, CheckStatus::Failure);
+        assert_eq!(
+            result.request_info.and_then(|i| i.http_status_code),
+            Some(400)
+        );
+        assert_eq!(
+            result.status_reason.as_ref().map(|r| r.status_type),
+            Some(CheckStatusReasonType::Failure)
+        );
+        assert_eq!(
+            result.status_reason.map(|r| r.description),
+            Some("Got non 2xx status: 400 Bad Request".to_string())
+        );
+
+        head_mock.assert();
+    }
+
+    // TODO: Figure out how to simulate a DNS failure
+    // assert_eq!(check_domain(&client, "https://hjkhjkljkh.io/".to_string()).await, CheckResult::FAILURE(FailureReason::DnsError("failed to lookup address information: nodename nor servname provided, or not known".to_string())));
+}
--- src/producer.rs
+++ src/producer.rs
@@ -1,12 +1,10 @@
-use rust_arroyo::backends::kafka::config::KafkaConfig;
-use rust_arroyo::backends::kafka::producer::KafkaProducer;
-use rust_arroyo::backends::kafka::types::KafkaPayload;
+pub mod kafka_producer;
 
-use rust_arroyo::types::{Topic, TopicOrPartition};
-use sentry_kafka_schemas::{Schema, SchemaError};
+use sentry_kafka_schemas::SchemaError;
+
+use rust_arroyo::backends::ProducerError;
 
 use crate::types::result::CheckResult;
-use rust_arroyo::backends::{Producer, ProducerError};
 
 #[derive(Debug, thiserror::Error)]
 pub enum ExtractCodeError {
@@ -18,85 +16,6 @@ pub enum ExtractCodeError {
     Schema(#[from] SchemaError),
 }
 
-pub struct ResultProducer {
-    producer: KafkaProducer,
-    topic: TopicOrPartition,
-    schema: Schema,
-}
-
-impl ResultProducer {
-    pub fn new(topic_name: &str, config: KafkaConfig) -> Self {
-        let producer = KafkaProducer::new(config);
-        let topic = TopicOrPartition::Topic(Topic::new(topic_name));
-        let schema = sentry_kafka_schemas::get_schema("uptime-results", None).unwrap();
-
-        Self {
-            producer,
-            topic,
-            schema,
-        }
-    }
-
-    pub async fn produce_checker_result(
-        &self,
-        result: &CheckResult,
-    ) -> Result<(), ExtractCodeError> {
-        let json = serde_json::to_vec(result)?;
-        self.schema.validate_json(&json)?;
-
-        // TODO: Note that this will rarely fail directly here - mostly will only happen if the queue
-        // is full. We need to have a callback that tells us whether the other produces succeed so we
-        // know whether to mark the check as successful. We never want to just silently drop a result -
-        // we'd much prefer to retry it later and produce a miss, so that we at least know that we missed
-        // and don't have holes.
-        Ok(self
-            .producer
-            .produce(&self.topic, KafkaPayload::new(None, None, Some(json)))?)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-
-    use super::{ExtractCodeError, ResultProducer};
-    use crate::types::result::{
-        CheckResult, CheckStatus, CheckStatusReason, CheckStatusReasonType, RequestInfo,
-        RequestType,
-    };
-    use chrono::{TimeDelta, Utc};
-    use rust_arroyo::backends::kafka::config::KafkaConfig;
-    use sentry::protocol::{SpanId, TraceId};
-    use uuid::{uuid, Uuid};
-
-    pub async fn send_result(result: CheckStatus) -> Result<(), ExtractCodeError> {
-        let guid = Uuid::new_v4();
-        let result = CheckResult {
-            guid,
-            subscription_id: uuid!("23d6048d67c948d9a19c0b47979e9a03"),
-            status: result,
-            status_reason: Some(CheckStatusReason {
-                status_type: CheckStatusReasonType::DnsError,
-                description: "hi".to_string(),
-            }),
-            trace_id: TraceId::default(),
-            span_id: SpanId::default(),
-            scheduled_check_time: Utc::now(),
-            actual_check_time: Utc::now(),
-            duration: Some(TimeDelta::seconds(1)),
-            request_info: Some(RequestInfo {
-                request_type: RequestType::Head,
-                http_status_code: Some(200),
-            }),
-        };
-        // TODO: Have an actual Kafka running for a real test. At the moment this is fine since
-        // it will fail async
-        let config = KafkaConfig::new_config(["0.0.0.0".to_string()].to_vec(), None);
-        let producer = ResultProducer::new("uptime-results", config);
-        producer.produce_checker_result(&result).await
-    }
-
-    #[tokio::test]
-    async fn test() {
-        let _ = send_result(CheckStatus::Success).await;
-    }
+pub trait ResultsProducer {
+    fn produce_checker_result(&self, result: &CheckResult) -> Result<(), ExtractCodeError>;
 }
