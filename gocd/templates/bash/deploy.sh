6e9013a1e49ba539591c680029f3a0345ea5c7f8
mchen-sentry <ming.chen@sentry.io>
2025-07-15 12:00:44 -0700

    fix(gocd): deploy.sh bash syntax error (#360)

--- gocd/templates/bash/deploy.sh
+++ gocd/templates/bash/deploy.sh
@@ -2,8 +2,7 @@
 
 eval $(regions-project-env-vars --region="${SENTRY_REGION}")
 
-/devinfra/scripts/get-cluster-credentials &&
-	k8s-deploy \
+/devinfra/scripts/get-cluster-credentials && k8s-deploy \
 		--type="statefulset" \
 		--label-selector="${LABEL_SELECTOR}" \
 		--image="us-central1-docker.pkg.dev/sentryio/uptime-checker/image:${GO_REVISION_UPTIME_CHECKER_REPO}" \
