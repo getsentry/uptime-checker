1e372aa3b54e53b2692d5494db79075f397ff5a9
Chris Klochek <christopher.klochek@sentry.io>
2025-11-17 12:41:09 -0500

    import assertions

--- src/types/check_config.rs
+++ src/types/check_config.rs
@@ -1,5 +1,5 @@
 use super::shared::{RegionScheduleMode, RequestMethod};
-use crate::config_store::Tick;
+use crate::{assertions, config_store::Tick};
 use chrono::{TimeDelta, Timelike};
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
@@ -80,10 +80,10 @@ pub struct CheckConfig {
     pub region_schedule_mode: Option<RegionScheduleMode>,
 
     #[serde(default)]
-    pub assertion: Option<crate::assertions::Assertion>,
+    pub assertion: Option<assertions::Assertion>,
 
     #[serde(skip)]
-    pub compiled_assertion: Option<crate::assertions::compiled::Assertion>,
+    pub compiled_assertion: Option<assertions::compiled::Assertion>,
 }
 
 impl Hash for CheckConfig {
